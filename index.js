0d55071059425c17d25c369cc5dfb0ac4e05da00
Samuel Yang <sam.yang@utexas.edu>
2016-04-10 16:21:53 -0500

    Fixed nav links and added node-garden

--- index.js
+++ index.js
@@ -1,91 +1,146 @@
-var looper;
-var SCREEN_WIDTH = 3000;
-var SCREEN_HEIGHT = 2000;
-var canvas = document.getElementById('c');
-var context = canvas.getContext('2d');
+var garden = document.getElementById("garden");
+garden.width = window.innerWidth;
+garden.height = window.innerHeight;
+var c = garden.getContext("2d");
+garden.style.background = 'rgba(0,0,0,0)';
 
-var nodes;
-var NUM_NODES = 200;
-var minDist = 100;
-var springAmount = 0.0001;
+c.imageSmoothingEnabled = false;
 
-//nodes_init();
+var numNodes = 500;
+var minLinkDist = 50;
+var screenWidth = window.innerWidth;
+var screenHeight = window.innerHeight;
 
-function nodes_init() {
-    createNodes();
-	context.lineWidth = 1;
-	looper = setInterval(nodes_loop, 1000/31);
+devicePixelRatio = window.devicePixelRatio || 1,
+backingStoreRatio = c.webkitBackingStorePixelRatio ||
+                    c.mozBackingStorePixelRatio ||
+                    c.msBackingStorePixelRatio ||
+                    c.oBackingStorePixelRatio ||
+                    c.backingStorePixelRatio || 1,
+
+ratio = devicePixelRatio / backingStoreRatio;
+if (devicePixelRatio !== backingStoreRatio) {
+    var oldWidth = garden.width;
+    var oldHeight = garden.height;
+
+    garden.width = oldWidth * ratio;
+    garden.height = oldHeight * ratio;
+
+    garden.style.width = oldWidth + 'px';
+    garden.style.height = oldHeight + 'px';
+
+    c.scale(ratio, ratio);
 }
 
-function createNodes() {
-	nodes = [];
-	for (var i=0; i<NUM_NODES; i++) {
+var nodes = [];
+
+function initNodes() {
+	for (var i = 0; i < numNodes; i++) {
 		var node = {
-			radius: 2,
-			x: Math.random()*SCREEN_WIDTH,
-			y: Math.random()*SCREEN_HEIGHT,
-			vx: Math.random()*1,
-			vy: Math.random()*1,
-			mass: 1,
-			update: function() {
-				this.x += this.vx;
-				this.y += this.vy;
-				if (this.x > SCREEN_WIDTH) {
-					this.x = 0;
-				} else if (this.x < 0) {
-					this.x = SCREEN_WIDTH;
-				}
-				if (this.y > SCREEN_HEIGHT) {
-					this.y = 0;
-				} else if (this.y < 0) {
-					this.y = SCREEN_HEIGHT;
-				}
-			},
-			draw: function() {
-				context.fillStyle = "	#696969";
-				context.beginPath();
-				context.arc(this.x, this.y, this.radius, 0, Math.PI*2, true);
-				context.closePath();
-				context.fill();
-			}
+			x: Math.random() * (screenWidth + 2 * minLinkDist),
+			y: Math.random() * (screenHeight + 2 * minLinkDist),
+			vx: Math.random() * .2+ .2,
+			vy: Math.random() * .2 + .2,
+			size: 1,
+			opacity: "rgba(63, 81, 181, 1)"
 		};
 		nodes.push(node);
-	}
+	}	
 }
 
-function nodes_loop() {
-	context.clearRect(0, 0, canvas.width, canvas.height);
-    for (i=0; i<NUM_NODES; i++) {
-        nodes[i].update();
-    	nodes[i].draw();
-    }
-	for (i=0; i<NUM_NODES-1; i++) {
-		var node1 = nodes[i];
-		for (var j=i+1; j<NUM_NODES; j++) {
-			var node2 = nodes[j];
-			spring(node1, node2);
+function update(nArray) {
+	for (var i = 0; i < nArray.length; i++) {
+		nArray[i].x += nArray[i].vx;
+		nArray[i].y += nArray[i].vy;
+		if (nArray[i].x > (screenWidth + minLinkDist) || nArray[i].y > (screenHeight + minLinkDist)) {
+			if (Math.random() < screenWidth/(screenHeight + screenWidth)) {
+				nArray[i].x = Math.random() * screenWidth;
+				nArray[i].y = -minLinkDist;
+			}
+			else {
+				nArray[i].y = Math.random() * screenHeight;
+				nArray[i].x = -minLinkDist;
+			}
 		}
 	}
 }
 
-function spring(na, nb) {
-	var dx = nb.x - na.x;
-	var dy = nb.y - na.y;
-	var dist = Math.sqrt(dx*dx + dy*dy);
-    
-    //If the distance between 2 nodes are smaller than minimum, connect them
-	if (dist<minDist) {
-		context.beginPath();
-		context.strokeStyle = "rgba(105,105,105	,"+(1-dist/minDist)+")";
-		context.moveTo(na.x, na.y);
-		context.lineTo(nb.x, nb.y);
-		context.stroke();
-		context.closePath();
-		var ax = dx*springAmount;
-		var ay = dy*springAmount;
-		na.vx += ax;
-		na.vy += ay;
-		nb.vx -= ax;
-		nb.vy -= ay;
+function draw(nArray) {
+	for (var i = 0; i < nArray.length; i++) {
+		c.beginPath();
+		c.arc(nArray[i].x, nArray[i].y, nArray[i].size, 0, Math.PI*2, true);
+		c.fillStyle = nArray[i].opacity;
+		c.fill();
+		for (var j = 0; j < nArray.length; j++) {
+			var dist = calcDist(nArray[i], nArray[j]);
+			if (dist < minLinkDist) {
+				c.beginPath();
+				c.moveTo(nArray[i].x, nArray[i].y);
+				c.lineTo(nArray[j].x, nArray[j].y);
+				c.lineWidth = 1;
+				c.strokeStyle = "rgba(63, 81, 181, " + ((minLinkDist - dist)/minLinkDist)  + ")";
+				c.stroke();
+			}
+		}
 	}
 }
+function calcDist(node1, node2) {
+	return Math.sqrt((node1.x - node2.x)*(node1.x - node2.x) + (node1.y - node2.y)*(node1.y - node2.y));
+}
+
+function drawupdate() {
+		c.clearRect(0, 0, screenWidth, screenHeight);
+		draw(nodes);
+		update(nodes);
+		requestAnimationFrame(drawupdate);
+
+}
+window.addEventListener('resize', resizeCanvas, false);
+window.addEventListener('click', addNode);
+
+function resizeCanvas() {
+	garden.width = window.innerWidth;
+	garden.height = window.innerHeight;
+	if (devicePixelRatio !== backingStoreRatio) {
+	    var oldWidth = garden.width;
+	    var oldHeight = garden.height;
+
+	    garden.width = oldWidth * ratio;
+	    garden.height = oldHeight * ratio;
+
+	    garden.style.width = oldWidth + 'px';
+	    garden.style.height = oldHeight + 'px';
+
+	    c.scale(ratio, ratio);
+	}
+}
+
+function addNode() {
+	var node = {
+		x: event.clientX,
+		y: event.clientY,
+		vx: Math.random() * .5+ .2,
+		vy: Math.random() * .5 + .2,
+		size: 1,
+		opacity: "rgba(63, 81, 181, 1)"
+	}
+	c.beginPath();
+	c.arc(node.x, node.y, node.size, 0, Math.PI*2, true);
+	c.fillStyle = node.opacity;
+	c.fill();
+	for (var i = 0; i < nodes.length; i++) {
+			var dist = calcDist(nodes[i], node);
+			if (dist < minLinkDist) {
+				c.beginPath();
+				c.moveTo(nodes[i].x, nodes[i].y);
+				c.lineTo(node.x, node.y);
+				c.lineWidth = .5;
+				c.strokeStyle = "rgba(63, 81, 181, " + ((minLinkDist - dist)/minLinkDist)  + ")";
+				c.stroke();
+			}
+		}
+	nodes.push(node);
+}
+
+initNodes();
+requestAnimationFrame(drawupdate);
